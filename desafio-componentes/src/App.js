2b839828f07670078c28e3cbeb7a4159e03be3ba
José Mariotto <josemarioto@gmail.com>
2023-07-31 00:01:43 -0300

    created components

---
+++ desafio-componentes/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Produtos from './Produtos';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Home />
+     <Produtos />
+    </div>
+  );
+}
+
+export default App;
---
+++ desafio-componentes/src/Produtos.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Titulo from './Titulo'
+
+const Produtos = () => {
+  return (
+    <section>
+        <Titulo texto="Produtos"/>
+        <p>Essa é os produtos</p>
+    </section>
+  )
+}
+
+export default Produtos
